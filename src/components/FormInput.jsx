8a753ad8680a48575e96d351adb54c9251cb55be
Hammad Ul Hassan <hammadulhassanqureshi@gmail.com>
2023-07-19 15:21:39 +0500

    "Second Commit"

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import "./formInput.css";
+
+const FormInput = (props) => {
+  const[focused, setFocused] =useState(false)
+  const{ label, errorMessage, onChange, id, ...inputProps} =props;
+  
+  const handleFocus=(e)=>{
+    setFocused(true);
+  };
+  
+
+  return (
+    <div className='formInput'>
+        <label>{label}</label>
+        <input  {...inputProps} onChange={onChange} 
+         onBlur={handleFocus} //click and leave 
+         onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        } //one click only
+         focused={focused.toString()}/>
+        <span>{errorMessage}</span>
+    </div>
+  )
+}
+
+export default FormInput
